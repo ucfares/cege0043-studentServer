832e6d5e9d3050280dc13d8293fe9c4d496ab18c
ucfares <ucfares@ucl.ac.uk>
2019-03-11 12:52:15 -0400

    student server test

--- studentServer.js
+++ studentServer.js
@@ -10,6 +10,38 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
+
+var fs = require ('fs');
+var pg = require ('pg');
+
+var configtext =
+""+fs.readFilesSync("/home/studentuser/certs/postGISConnection.js");
+
+// now convert the configuration file into the correct format -i.e. a name/value pair array
+var configarray = configtext.split(",");
+var config = {};
+for (var i = 0; i < configarray.length; i++) {
+	var split = configarray[i].split(",");
+	config[split [0].trim()] = split[1].trim();
+}
+var pool = new pg.Pool(config); 
+
+app.get('/postgistest', function (req, res) {
+	pool.connect(function(err,client,done) {
+		if(err) {
+			console.log("not able to get connecion " +err);
+			res.status(400).send(err);
+		}
+		client.query('SELECT name FROM london_poi' ,function(err, result){
+			done();
+			if (err) {
+				console.log("not able to get connection "+ err);
+				res.status(400).send(err);
+			}
+			res.status(200).send(results.rows);
+		});
+	});
+});
 app.use(bodyParser.json());
 var http = require('http');
 var httpServer= http.createServer(app);
